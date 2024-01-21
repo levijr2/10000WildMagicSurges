58e56388030286928db8ad725e632cc4ef9ff423
Levi Gainer <98910841+levijr2@users.noreply.github.com>
2024-01-21 09:55:57 -0500

    Update burst_roller.py

--- burst_roller.py
+++ burst_roller.py
@@ -17,17 +17,3 @@ with open( CSV_FILE_PATH, "r") as csv_file:
         if csv_data.line_num==num:
             print(line)
 
-'''with open( CSV_FILE_PATH, "r") as csv_file:
-    f = csv.DictReader(csv_file)
-
-    filesize = 10000                 #size of the really big file
-    offset = random.randrange(filesize)
-
-    f.seek(offset)                  #go to random position
-    f.readline()                    # discard - bound to be partial line
-    random_line = f.readline()      # bingo!
-    # extra to handle last/first line edge cases
-    if len(random_line) == 0:       # we have hit the end
-        f.seek(0)
-        random_line = f.readline()  # so we'll grab the first line instead
-        print(random_line)'''
